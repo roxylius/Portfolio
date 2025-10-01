016b270e12fb62540e2f950a62a5eeb01915ee4d
Abhijeet Anand <98657313+roxylius@users.noreply.github.com>
2025-10-01 18:57:11 +0000

    fix import error

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from 'next';
 
 const isProd = process.env.NODE_ENV ==='production';
 
-import type { NextConfig } from 'next';
-
 const nextConfig: NextConfig = {
     /* config options here */
     output: isProd ? 'export' : 'standalone',
